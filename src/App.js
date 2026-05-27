18c60d3de26a1a04a676ebb470eeb33450700ac1
lysa.waitlist <Lysauniverse30@gmail.com>
2026-05-27 01:34:19 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-// App.js
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Register from "./pages/Register";
+import PaymentSuccess from "./pages/PaymentSuccess";
 
 function App() {
   return (
-    <div>
-      <Register />
-    </div>
+    <Router>
+      <Routes>
+        {/* REGISTER PAGE */}
+        <Route path="/" element={<Register />} />
+
+        {/* PAYMENT SUCCESS PAGE */}
+        <Route path="/payment-success" element={<PaymentSuccess />} />
+      </Routes>
+    </Router>
   );
 }
 
